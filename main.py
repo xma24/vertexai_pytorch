b24db83102dea529e2ae5efd9aa8b2d86ef4b4cb
xma24 <xma24@buffalo.edu>
2023-08-03 16:58:05 -0500

    udpate

--- main.py
+++ main.py
@@ -23,54 +23,55 @@ TENSORBOARD_ID = os.getenv("TENSORBOARD_ID")
 
 
 def deploy_training_pipeline(request):
-
     payload = request.get_data(as_text=True)
     args = json.loads(payload)
-    
+
     # example for args object
     # args = {
     #     "language": "en",
     # }
-    
+
     with open("cfg.yml", "r") as f:
         cfg = yaml.safe_load(f)
     pipeline_cfg = cfg["pipeline_cfg"]
     model_cfg = cfg["model_cfg"][args["language"]]
-    
-    cfg = argparse.Namespace(**{
-        **pipeline_cfg,
-        **model_cfg,
-        **args,
-        "project_id": PROJECT_ID,
-        "service_account": SERVICE_ACCOUNT,
-        "repo_branch": REPO_BRANCH,
-        "bucket": BUCKET,
-        "tensorboard_id": TENSORBOARD_ID
-    })
-    
+
+    cfg = argparse.Namespace(
+        **{
+            **pipeline_cfg,
+            **model_cfg,
+            **args,
+            "project_id": PROJECT_ID,
+            "service_account": SERVICE_ACCOUNT,
+            "repo_branch": REPO_BRANCH,
+            "bucket": BUCKET,
+            "tensorboard_id": TENSORBOARD_ID,
+        }
+    )
+
     dt = datetime.now().strftime("%Y%m%d_%H%M%S")
     working_dir = f"{cfg.bucket}/{cfg.gcs_training_pipeline_dir}/{cfg.language}/{cfg.language}_{dt}"
-    model_name_output = cfg.model_name.split('/')[-1] + ".pt" # name of model output file
+    model_name_output = (
+        cfg.model_name.split("/")[-1] + ".pt"
+    )  # name of model output file
     endpoint_model_name = f"{cfg.endpoint_model_name}_{cfg.language}"
-    
+
     setattr(cfg, "dt", dt)
     setattr(cfg, "working_dir", working_dir)
     setattr(cfg, "model_name_output", model_name_output)
     setattr(cfg, "endpoint_model_name", endpoint_model_name)
-    
 
     pipeline_params = {
         "comp_params": cfg.__dict__,
-        "model_info": {**args, **model_cfg, "working_dir": working_dir}
+        "model_info": {**args, **model_cfg, "working_dir": working_dir},
     }
 
     with tempfile.NamedTemporaryFile() as temp_f:
         pipeline_filename = f"{temp_f.name}.json"
         compiler.Compiler().compile(
-            pipeline_func=training_pipeline,
-            package_path=pipeline_filename
+            pipeline_func=training_pipeline, package_path=pipeline_filename
         )
-        
+
     response = aiplatform.PipelineJob(
         display_name=working_dir,
         template_path=pipeline_filename,
@@ -78,11 +79,11 @@ def deploy_training_pipeline(request):
         pipeline_root=working_dir,
         parameter_values=pipeline_params,
         project=cfg.project_id,
-        location=cfg.pipeline_job_location
+        location=cfg.pipeline_job_location,
     )
 
     response.submit()
 
     print("Successfully deployed training pipeline")
-    
+
     return "DONE"
--- training_pipeline/components/build_model/main.py
+++ training_pipeline/components/build_model/main.py
@@ -3,26 +3,24 @@ from typing import NamedTuple
 
 def build_model(
     comp_params: dict,
-) -> NamedTuple('build_model_outputs', [
-    ('endpoint_image_uri', str),
-    ('commit_sha', str)
-]):
-
+) -> NamedTuple(
+    "build_model_outputs", [("endpoint_image_uri", str), ("commit_sha", str)]
+):
     import os
     import argparse
     from google.cloud.devtools import cloudbuild
-    
+
     cfg = argparse.Namespace(**comp_params)
-    
+
     endpoint_image_uri = f"{cfg.build_location}-docker.pkg.dev/{cfg.project_id}/{cfg.endpoint_image_name}/{cfg.endpoint_image_name}_{cfg.language}"
 
     client = cloudbuild.CloudBuildClient()
-    
+
     request = cloudbuild.RunBuildTriggerRequest(
-        project_id = cfg.project_id,
-        trigger_id = cfg.trigger_name,
+        project_id=cfg.project_id,
+        trigger_id=cfg.trigger_name,
         # https://cloud.google.com/python/docs/reference/cloudbuild/latest/google.cloud.devtools.cloudbuild_v1.types.RepoSource
-        source = {
+        source={
             "branch_name": cfg.repo_branch,
             "dir_": cfg.build_dir,
             "substitutions": {
@@ -31,13 +29,13 @@ def build_model(
                 "_TRAINER_CODE_DIR": cfg.trainer_code_dir,
                 "_BUILD_DIR": cfg.build_dir,
                 "_MODEL_NAME": cfg.endpoint_model_name,
-                "_LABEL_MAP_FILE": cfg.output_file_label_map
-            }
-        }
+                "_LABEL_MAP_FILE": cfg.output_file_label_map,
+            },
+        },
     )
 
     operation = client.run_build_trigger(request=request)
-    
+
     response = operation.result()
-    
+
     return (endpoint_image_uri, response.substitutions["COMMIT_SHA"])
